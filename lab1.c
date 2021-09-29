dc5714b21ac91df9292ad0c8575da784fbbd3437
goluckywizard <73885997+goluckywizard@users.noreply.github.com>
2021-09-29 21:37:31 +0700

    Update lab1.c

--- lab1.c
+++ lab1.c
@@ -13,16 +13,18 @@ void *print(char *arg) {
         int i = 0;
         for (i = 0; i < 10; ++i)
                 printf("%s%d\n", arg, i);
+        free(arg);
         return EXIT_SUCCESS;
 }
 
-static char childText[10] = "child";
-
 int main(void) {
+        const int LENGTH_OF_ARG = 10;
         pthread_t thread;
+        char *childText = (char*)malloc(sizeof(char)*LENGTH_OF_ARG);
+        childText = "child";
         int err = pthread_create(&thread, NULL, print, childText);
         if (err != PTHREAD_CREATE_SUCCESS) {
-                fprintf(stderr, "Unable to create thread!");
+                perror("Unable to create thread!");
                 exit(EXIT_FAILURE);
         }
         print("parent");
